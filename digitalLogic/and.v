7501442ed73bff1b4f5d617f4a958c5631fa31dd
Naimul Hoque <naimulhoque@Naimuls-MBP.attlocal.net>
2021-08-25 23:05:35 -0700

    Naimul - Created And Gate in Verilog

---
+++ digitalLogic/and.v
@@ -0,0 +1,6 @@
+module AndGate (input a, input b, output out);
+    // & is bitwise, && is logical
+    // a,b, and out are wires.
+    // assign creates a connection between the wires.
+    assign out = a & b;
+end
