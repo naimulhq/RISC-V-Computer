a472fd238ece98c16f64705f64252f421409f7f8
naimulhq <naimul@ucsb.edu>
2021-08-26 17:02:13 -0700

    Naimul - Deleted original file

--- digitalLogic/and.v
+++
@@ -1,6 +0,0 @@
-module AndGate (input a, input b, output out);
-    // & is bitwise, && is logical
-    // a,b, and out are wires.
-    // assign creates a connection between the wires.
-    assign out = a & b;
-end
